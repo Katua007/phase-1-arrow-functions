990bd58b71bc188a2306c0a1953309d9df4ea388
katua <katuacyril@gmail.com>
2025-06-10 12:46:42 +0300

    arrow-functions

---
+++ index.js
@@ -0,0 +1,10 @@
+function divide (){
+    let a=2000;
+    let b=100;
+    return a / b;
+}
+const square = (x) => {return x * x;};
+console.log(square(5)); // 25
+
+const add = (x, y) => {return x + y;};
+console.log(add(5, 10)); // 15
